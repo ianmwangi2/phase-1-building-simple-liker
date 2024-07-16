bbb2f1a895eaf02f77d8159201158af19a596bb0
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-07-16 09:09:05 +0300

    modified index.html and main.js

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
 
+  function toggleHeart(event) {
+    const heart = event.target;
 
-
+    mimicServerCall()
+      .then(() => {
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {
+        const errorModal = document.getElementById('modal');
+        errorModal.innerText = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  }
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', toggleHeart);
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
